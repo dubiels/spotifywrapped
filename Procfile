3da801cc183762daca66cc531a7bc2ffa80f8bf2
Esther Park <esther514514@gmail.com>
2024-11-29 16:17:08 -0800

    Add migrations and update settings

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
-release: python manage.py makemigrations accounts && python manage.py migrate accounts
+release: python manage.py makemigrations accounts && python manage.py migrate accounts && python manage.py makemigrations && python manage.py migrate
 web: gunicorn spotifyWrapped.wsgi --log-file -
--- spotifyWrapped/settings.py
+++ spotifyWrapped/settings.py
@@ -90,13 +90,21 @@ WSGI_APPLICATION = 'spotifyWrapped.wsgi.application'
 #     }
 # }
 
-DATABASES = {
-    'default': dj_database_url.config(
-        default='postgres://',
-        conn_max_age=600,
-        ssl_require=True
-    )
-}
+if os.getenv('DATABASE_URL'):
+    DATABASES = {
+        'default': dj_database_url.config(
+            conn_max_age=600,
+            ssl_require=True
+        )
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
+
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
 
